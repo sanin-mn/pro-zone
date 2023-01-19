a317331e368a0ef946bf211582733e922c5d7bcf
sanin <msanin7003@gmail.com>
2023-01-19 22:31:13 +0530

    home screen & product screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,23 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Fashion kart</a>
-      </header>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Fashion cart</Link>
+        </header>
 
-      <main>
-        <h1> Featured products </h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button> Add to Cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
